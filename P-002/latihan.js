69376fc2a0bc367aa9d0290e9d3da0488e48531a
Aries-Indrabayu <indra13498@gmail.com>
2025-06-03 02:33:03 +0700

    P1-P3

---
+++ P-002/latihan.js
@@ -0,0 +1,8 @@
+console.log("Selamat belajar JavaScript!");
+
+// Ubah teks di console.log() menjadi nama kamu:
+console.log("Halo, saya Bayu!");
+
+// Tambahkan dua baris pesan lainnya untuk membiasakan tangan mengetik:
+console.log("Hari ini belajar JavaScript.");
+console.log("Semangat terus ya!");
---
+++ P-002/latihan_komentar.js
@@ -0,0 +1,10 @@
+// Program sederhana menampilkan 2 pesan
+
+/*
+Dibuat oleh: [Nama Kamu]
+Tanggal: [Tanggal Hari Ini]
+Tujuan: Latihan komentar dan console.log()
+*/
+
+console.log("Hai, saya sedang belajar JavaScript!");
+console.log("Komentar sangat membantu!");
